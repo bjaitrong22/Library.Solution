d344d9938821e514daaf7b8d23f0b04013dbfa57
Bai Jaitrong <bjaitrong@comcast.net>
2023-01-18 22:33:51 -0700

    Add Index action and view.
    Co-authored-by: Rafael Petrachini <raffp@me.com>

---
+++ Library/Controllers/BooksController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using System.Linq;
+using Library.Models;
+
+
+namespace Library.Controllers
+{
+  public class BooksController: Controller
+  {
+    private readonly LibraryContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public BooksController(UserManager<ApplicationUser> userManager, LibraryContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {  
+      List<Book> books = _db.Books
+        .Include(book => book.Catalog)
+        .ToList();
+
+      return View(books);
+    }
+  }
+}
