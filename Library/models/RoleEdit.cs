2345f30744ebc9cd60fa126c7affcc20af9e0275
Bai Jaitrong <bjaitrong@comcast.net>
2023-01-23 13:49:59 -0700

    Add RoleEdit class, represent the Role and details of Users in the Role.
    Co-authored-by: Rafael Petrachini <raffp@me.com>

---
+++ Library/models/RoleEdit.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using Library.Models;
+
+namespace Library.Models
+{
+  public class RoleEdit
+  {
+    public IdentityRole Role { get; set; }
+    public IEnumerable<ApplicationUser> Members { get; set; }
+    public IEnumerable<ApplicationUser> Nonmembers { get; set; }
+  }
+}
